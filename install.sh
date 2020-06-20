2c9dc27680bbb5f80487819f28c0a67d53837583
Joseba Martos <josebamartos@users.noreply.github.com>
2020-06-21 01:02:26 +0200

    Fixing permission problems in the installer

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ $(id -u) -ne 0 ]]; then echo "Run as root"; exit 1; fi
-
 if [[ -d ${HOME}/.config/vsp ]]; then
     echo "Configuration directory already exists at ${HOME}/.config/vsp"
     echo "Remove directory ${HOME}/.config/vsp to reinstall vsp"
@@ -11,6 +9,6 @@ fi
 
 mkdir ${HOME}/.config/vsp
 curl -Ls "https://raw.githubusercontent.com/josebamartos/vsp/master/config.json" -o ${HOME}/.config/vsp/config.json
-curl -Ls "https://raw.githubusercontent.com/josebamartos/vsp/master/vsp.sh" -o /usr/local/bin/vsp
+sudo curl -Ls "https://raw.githubusercontent.com/josebamartos/vsp/master/vsp.sh" -o /usr/local/bin/vsp
 chmod +x /usr/local/bin/vsp
 echo "Installation done"
